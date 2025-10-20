4b99e7e2e0d88380ca141e4d25f50e6e3cb79098
Matheus Diedrichs <136713101+MatheusDDChs@users.noreply.github.com>
2025-10-19 22:36:39 -0300

    Redirects for host.

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import VideoShowPage from './pages/VideoShowPage.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App />,
   },
-  {
-    path: '/video',
-    element: <VideoShowPage/>
-  }
-])
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
